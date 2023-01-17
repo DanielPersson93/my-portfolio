2e3e46630249ae0d2feeb8bb88abb32afc9da6a5
danielpersson93 <daniel.persson93@hotmail.se>
2023-01-17 11:55:15 +0100

    contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     resolve: 'gatsby-source-contentful',
     options: {
       "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
-      "spaceId": process.env.SPACE_ID
+      "spaceId": process.env.CONTENTFUL_SPACE_ID
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components"]
 };
